2db255962ee7fd6329c587373506911b34147648
Michael Steshenko <19752384+Michael-Steshenko@users.noreply.github.com>
2025-03-16 19:05:58 +0200

    correct shape and spawn pos of L block

--- src/TetrisPieces/LBlock.cpp
+++ src/TetrisPieces/LBlock.cpp
@@ -2,8 +2,8 @@
 
 LBlock::LBlock() {
     setBlockType(BlockType::L);
-    coords[0] = Point(0, 6);
-    coords[1] = Point(0, 8);
-    coords[2] = Point(0, 10);
-    coords[3] = Point(0, 12);
+    coords[0] = Point(0, 8);
+    coords[1] = Point(0, 10);
+    coords[2] = Point(0, 12);
+    coords[3] = Point(1, 8);
 }
